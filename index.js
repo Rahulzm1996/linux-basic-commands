84a0b3d0abf7b70543e1abf12facd0d19e2140aa
Rahul Chauan <80570138+Rahulzm1996@users.noreply.github.com>
2024-07-27 16:04:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-1.hostname
-2.whoami
+1.hostname - hostname
+2.whoami - name
+3.ip addr - ip address
+4.pwd - current working directory
+5.ls - gives all list of files and folders
+6. mkdir folderName - to create folder
+7.cd myFolder - to go inside myFolder
+8.clear - clear termail
+9.history - show history of previous commands
+10. find currentFolder -name (serach by name) folderNameToFind
+11. ls commonName* (list all the foler whose name have common text in name)
+12. touch fileName - to create a file
+13. rmdir folderName OR rm -r folderName  - to delete a folder
+14. ls -l - to get details about files/folders
+15. vi fileName - to open file in VI Editor and press i to go into insert mode edit and press ESC to done with editing and :wq for exit from VI.
+16. :wq  - save and quit
+17. :q - quit without save
+18. ls - ltr (l-file details, t - time details, r - reverse order)
+19. cat fileName - to read a file content
+20. less fileName - to read the complete file at once
+21. diff fileName1 fileName2  - to check the diff between files
+22. cp fileName destination - copy file from current to destination
